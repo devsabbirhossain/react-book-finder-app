1f3513db4593776767d56d19f661ec2c00fd4b0b
devsabbirhossain <sabbir2dev@gmail.com>
2024-03-27 03:17:10 +0600

    resolve path case sensetive issue

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { Products } from "../data/producs.js";
-import ProductList from "./Shop/ProductList";
-import ShopHeader from "./Shop/ShopHeader";
+import { Products } from "../data/products.js";
+import ProductList from "./Shop/ProductList.jsx";
+import ShopHeader from "./Shop/ShopHeader.jsx";
 
 const Shop = () => {
   const [products, setProducts] = useState(Products);
